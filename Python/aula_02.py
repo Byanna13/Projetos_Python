e40a1b094373ea5a3ea27f1b2cc179b72006b7e6
Fabiana de Souza Silva <fabiana.souza@academico.ifrn.edu.br>
2022-09-29 09:58:19 -0300

    atualizando

--- Python/aula_02.py
+++ Python/aula_02.py
@@ -2,7 +2,23 @@ print("Hello World")
 
 #29 de setembro de 2022
 
+"""
 numero = int(input("Digite um número inteiro: "))
 
-if (numero%2) == 0: print("Número digitado é Par!")
+if numero%2 == 0: print("Número digitado é Par!")
 else: print("Número não é Par")
+
+numero = int(input("Digite um número entre 1 e 10: "))
+
+if numero>1 and numero<10: print("Número esta no intervalo: ", numero)
+else: print("Número não esta no intervalo",numero) 
+
+"""
+
+numero = int(input("Digite um numero: "))
+if numero==0: 
+    print("Número Zero ", numero)
+elif numero > 0:
+    print("Número Positivo ", numero)
+else: 
+    print("Número Negativo ",numero)
